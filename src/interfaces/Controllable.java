1e66e7b3af773114d67d5732eeb7b17aa7a7baeb
yismin <yasminetbessi59@gmail.com>
2025-12-15 19:48:28 +0100

    structure added:  main, structure, interfaces, exceptions

---
+++ src/interfaces/Controllable.java
@@ -0,0 +1,14 @@
+package interfaces;
+public interface Controllable {
+    /**
+     * Executes a command on the device
+     * @param command The command to execute
+     */
+    void executeCommand(String command);
+
+    /**
+     * Sets the operating mode of the device
+     * @param mode The mode to set
+     */
+    void setMode(String mode);
+}
